f258d52d5998e8ccd7b3697a5e0806f3c4620a3a
Kenneth Jiang <kenneth.jiang@gmail.com>
2022-05-17 05:59:57 +0100

    Move server_conn to a separate file to further trim app.py

--- moonraker_obico/app.py
+++ moonraker_obico/app.py
@@ -22,6 +22,7 @@ from .logger import getLogger, setup_logging
 from .printer import PrinterState
 from .config import MoonrakerConfig, ServerConfig, Config
 from .moonraker_conn import MoonrakerConn
+from .server_conn import ServerConn
 
 
 logger = getLogger()
@@ -42,108 +43,6 @@ ACKREF_EXPIRE_SECS = 300
 def fix_timestamp(cur_ts, now_ts):
     # force resetting timestamp when time skews
     return 0 if cur_ts > now_ts else cur_ts
-
-
-class ServerConn(ConnHandler):
-    max_backoff_secs = 300
-    flow_step_timeout_msecs = 5000
-
-    def __init__(self, id, sentry, tsd_config, on_event):
-        super().__init__(id, sentry, on_event)
-        self.config: ServerConfig = tsd_config
-
-    def flow(self):
-        self.timer.reset(None)
-        self.ready = False
-
-        if self.conn:
-            self.conn.close()
-
-        self.logger.debug('fetching printer data')
-        linked_printer = self._get_linked_printer()
-        self.on_event(
-            Event(sender=self.id, name='linked_printer', data=linked_printer)
-        )
-
-        self.conn = WSConn(
-            id=self.id,
-            auth_header_fmt='authorization: bearer {}',
-            sentry=self.sentry,
-            url=self.config.ws_url(),
-            token=self.config.auth_token,
-            on_event=self.push_event,
-            logger=getLogger(f'{self.id}.ws'),
-        )
-
-        self.conn.start()
-
-        self.logger.debug('waiting for connection')
-        self.wait_for(self._received_connected)
-
-        self.set_ready()
-        self.logger.info('connection is ready')
-        self.on_event(
-            Event(sender=self.id, name=f'{self.id}_ready', data={})
-        )
-
-        self.loop_forever(self.on_event)
-
-    def _get_linked_printer(self):
-        if not self.config.auth_token:
-            raise FlowError('auth_token not configured')
-
-        try:
-            resp = self.send_http_request(
-                'GET',
-                '/api/v1/octo/printer/',
-            )
-        except Exception as exc:
-            raise FlowError('failed to fetch printer', exc=exc)
-
-        return resp.json()['printer']
-
-    def _received_connected(self, event):
-        if event.name == 'connected':
-            return True
-
-    def send_status_update(self, data):
-        if self.ready:
-            self.conn.send(data)
-
-    def send_http_request(
-        self, method, uri, timeout=10, raise_exception=True,
-        **kwargs
-    ):
-        endpoint = self.config.canonical_endpoint_prefix() + uri
-        headers = {
-            'Authorization': f'Token {self.config.auth_token}'
-        }
-        headers.update(kwargs.pop('headers', {}))
-
-        _kwargs = dict(allow_redirects=True)
-        _kwargs.update(kwargs)
-
-        self.logger.debug(f'{method} {endpoint}')
-        try:
-            resp = requests.request(
-                method, endpoint, timeout=timeout, headers=headers, **_kwargs)
-        except Exception:
-            if raise_exception:
-                raise
-            return None
-
-        if raise_exception:
-            # if resp.status_code in (401, 403):
-            #     raise AuthenticationError(
-            #             f'HTTP {resp.status_code}',
-            #             exc=resp_to_exception(resp))
-            resp.raise_for_status()
-
-        return resp
-
-    def send_passthru(self, payload: Dict):
-        if self.ready:
-            self.conn.send({'passthru': payload})
 
 
 class App(object):
