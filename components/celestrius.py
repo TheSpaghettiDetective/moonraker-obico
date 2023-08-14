7cdd15a09f748365ed7fc6f01549b73ef3fc9908
aarontuftss <aarontufts.dev@gmail.com>
2023-08-14 15:51:30 -0600

    component setup

--- components/celestrius.py
+++ components/celestrius.py
@@ -1,28 +1,32 @@
 class Celestrius:
     def __init__(self, config):
         self.server = config.get_server()
-        self.name = config.get_name()
+        self.celestrius_compatible = None,
+        self.celestrius_url = None,
+        self.images = []
 
-        # Raises an error if "example_int_option" is not configured in
-        # the [example] section
-        self.example_int_opt = config.getint("example_int_option")
+        self.server.register_endpoint("/server/celestrius_config", ['POST'],
+                                      self._load_config)
+        self.server.register_endpoint("/server/reset_celestrius", ['POST'],
+                                      self._reset_celestrius)
+        self.server.register_endpoint("/server/celestrius_config", ['POST'],
+                                      self._load_config)
 
-        # Returns a NoneType if "example_float_option is not configured
-        # in the config
-        self.example_float_opt = config.getfloat("example_float_option", None)
+    async def _load_config(self, web_request):
+        self.celestrius_compatible = web_request.get_int("celestrius_compatible")
+        self.celestrius_url = web_request.get_int("celestrius_url")
+        return 'ok'
+    
+    async def _reset_celestrius(self):
+        return # TODO 
 
-        self.server.register_endpoint("/server/example", ['GET'],
-                                      self._handle_example_request)
+    
+    async def take_img(self):
+        return # TODO 
 
-    async def request_some_klippy_state(self):
-        klippy_apis = self.server.lookup_component('klippy_apis')
-        return await klippy_apis.query_objects({'print_stats': None})
+    async def dump_images_to_server(self):
+        return # TODO 
 
-    async def _handle_example_request(self, web_request):
-        web_request.get_int("required_reqest_param")
-        web_request.get_float("optional_request_param", None)
-        state = await self.request_some_klippy_state()
-        return {"example_return_value": state}
 
 def load_component(config):
     return Celestrius(config)
