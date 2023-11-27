6674d67b8ce94302a75ebe6772c1b9f7e0f896b0
Kenneth Jiang <kenneth.jiang@gmail.com>
2023-11-26 19:02:10 -0800

    K1 has a very small root file system that may get clogged up by pip cache

--- scripts/install_creality.sh
+++ scripts/install_creality.sh
@@ -31,10 +31,10 @@ ensure_deps() {
   report_status "Installing required system packages..."
   PKGLIST="python3 python3-pip"
   opkg install ${PKGLIST}
-  pip3 install virtualenv
+  pip3 install -q --no-cache-dir virtualenv
   ensure_venv
-  debug Running... "${OBICO_ENV}"/bin/pip3 install -q -r "${OBICO_DIR}"/requirements.txt
-  "${OBICO_ENV}"/bin/pip3 install -q --require-virtualenv -r "${OBICO_DIR}"/requirements.txt
+  debug Running... "${OBICO_ENV}"/bin/pip3 install -q --require-virtualenv --no-cache-dir -r "${OBICO_DIR}"/requirements.txt
+  "${OBICO_ENV}"/bin/pip3 install -q --require-virtualenv --no-cache-dir -r "${OBICO_DIR}"/requirements.txt
   echo ""
 }
 
