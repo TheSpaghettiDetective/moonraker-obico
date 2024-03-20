eee5019abc49338c6877fdca934a7a05683b238b
Kenneth Jiang <kenneth.jiang@gmail.com>
2024-03-19 20:55:14 -0700

    Bug from blindedly trusting copilot

--- moonraker_obico/webcam_stream.py
+++ moonraker_obico/webcam_stream.py
@@ -58,7 +58,7 @@ class WebcamStreamer:
 
     @backoff.on_exception(backoff.expo, Exception)
     def mjpeg_loop(self):
-        min_interval_btw_frames = 1.0 / self.webcam_config.get_target_fps(fallback_fps=3)
+        min_interval_btw_frames = 1.0 / self.config.webcam.get_target_fps(fallback_fps=3)
         bandwidth_throttle = 0.004
 
         self.mjpeg_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
