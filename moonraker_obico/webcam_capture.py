06d89a898cf8e1decc27a5cb0fb7272bd3337847
B <bnance@tracer.tech>
2024-04-18 15:59:02 -0400

    update MjpegStreamChunker to not parse empty lines as the mjpeg boundary

--- moonraker_obico/webcam_capture.py
+++ moonraker_obico/webcam_capture.py
@@ -92,11 +92,15 @@ class MjpegStreamChunker:
         #         None: in the middle of the chunk
         # The first time endOfChunk should be called
         # with 'boundary' text as input
+        if not len(line.strip()): # don't parse empty lines as the boundary
+            self.current_chunk.write(line)
+            return None
+        
         if not self.boundary:
             self.boundary = line
             self.current_chunk.write(line)
             return None
-
+        
         if len(line) == len(self.boundary) and line == self.boundary:
             # start of next chunk
             return self.current_chunk.getvalue()
