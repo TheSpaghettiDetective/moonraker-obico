9b2b23fe6fbe68a51a773bbc01e7ced94096df21
Kenneth Jiang <kenneth.jiang@gmail.com>
2026-09-16 06:57:18 -0700

    Detect h264_nvmpi hardware encoder for NVIDIA Jetson
    Jetson/Tegra boards support none of the encoders probed at startup, so
    webcam streaming fell back to MJPEG. Probe h264_nvmpi (from jetson-ffmpeg)
    last so other boards are unaffected.
    
    Fixes #134

--- moonraker_obico/webcam_stream.py
+++ moonraker_obico/webcam_stream.py
@@ -74,7 +74,7 @@ def find_ffmpeg_h264_encoder():
     test_video = os.path.join(FFMPEG_DIR, 'test-video.mp4')
     FNULL = open(os.devnull, 'w')
     try:
-        for encoder in ['h264_omx', 'h264_v4l2m2m', 'h264_rkmpp']:
+        for encoder in ['h264_omx', 'h264_v4l2m2m', 'h264_rkmpp', 'h264_nvmpi']:
             ffmpeg_cmd = '{} -re -i {} -pix_fmt yuv420p -vcodec {} -an -f rtp rtp://127.0.0.1:8014?pkt_size=1300'.format(FFMPEG, test_video, encoder)
             _logger.debug('Popen: {}'.format(redact_text(ffmpeg_cmd)))
             ffmpeg_test_proc = subprocess.Popen(ffmpeg_cmd.split(' '), stdout=FNULL, stderr=FNULL)
